3735bc642d1af593751e7d2792cc40bc38e45f6b
Komal Suryan <59725952+komalsuryan@users.noreply.github.com>
2022-12-09 15:15:17 +0530

    :wrench: Update LoginView
    - add labels to indicate errors in email and password

--- src/main/java/org/amishaandkomal/views/LoginView.java
+++ src/main/java/org/amishaandkomal/views/LoginView.java
@@ -10,6 +10,8 @@ public class LoginView {
     private JButton signInButton;
     private JLabel emailLabel;
     private JLabel passwordLabel;
+    private JLabel emailErrorLabel;
+    private JLabel passwordErrorLabel;
 
     public LoginView() {
         // assign label text
@@ -22,15 +24,34 @@ public class LoginView {
 
         // assign button action
         loginButton.addActionListener(e -> {
+            if (emailTextField.getText().isEmpty()) {
+                emailErrorLabel.setText("Email cannot be empty");
+                emailErrorLabel.setVisible(true);
+                return;
+            }
+            if (String.valueOf(passwordTextField.getPassword()).isEmpty()) {
+                passwordErrorLabel.setText("Password cannot be empty");
+                passwordErrorLabel.setVisible(true);
+                return;
+            }
             if (emailTextField.getText().equals("admin") && String.valueOf(passwordTextField.getPassword()).equals("admin")) {
                 JOptionPane.showMessageDialog(null, "Login Successful", "Success", JOptionPane.INFORMATION_MESSAGE);
             } else {
                 JOptionPane.showMessageDialog(null, "Invalid Credentials", "Error", JOptionPane.ERROR_MESSAGE);
             }
         });
+        signInButton.addActionListener(e -> JOptionPane.showMessageDialog(null, "Sign In Successful", "Success", JOptionPane.INFORMATION_MESSAGE));
 
-        signInButton.addActionListener(e -> {
-            JOptionPane.showMessageDialog(null, "Sign In Successful", "Success", JOptionPane.INFORMATION_MESSAGE);
+        // assign error label listeners
+        emailTextField.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                emailErrorLabel.setVisible(false);
+            }
+        });
+        passwordTextField.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                passwordErrorLabel.setVisible(false);
+            }
         });
     }
 
