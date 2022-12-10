d10b6e479b64b3367bdae2a3dcf35bb597418821
Komal Suryan <59725952+komalsuryan@users.noreply.github.com>
2022-12-10 15:22:41 +0530

    :wrench: Update LoginView
    - added credential verification
    - assigned enter key to login
    - refactored signin button to signup button

--- src/main/java/org/amishaandkomal/views/LoginView.java
+++ src/main/java/org/amishaandkomal/views/LoginView.java
@@ -1,13 +1,21 @@
 package org.amishaandkomal.views;
 
+import com.password4j.Password;
+import org.amishaandkomal.DatabaseSetup;
+
 import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class LoginView {
     private JPanel mainPanel;
     private JTextField emailTextField;
     private JPasswordField passwordTextField;
     private JButton loginButton;
-    private JButton signInButton;
+    private JButton signUpButton;
     private JLabel emailLabel;
     private JLabel passwordLabel;
     private JLabel emailErrorLabel;
@@ -20,27 +28,14 @@ public class LoginView {
 
         // assign button text
         loginButton.setText("Login to Continue");
-        signInButton.setText("Not a registered user? Sign Up");
+        signUpButton.setText("Not a registered user? Sign Up");
+
+        // assign enter key to 'login' button
+        mainPanel.registerKeyboardAction(e -> loginButton.doClick(), KeyStroke.getKeyStroke("ENTER"), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
         // assign button action
-        loginButton.addActionListener(e -> {
-            if (emailTextField.getText().isEmpty()) {
-                emailErrorLabel.setText("Email cannot be empty");
-                emailErrorLabel.setVisible(true);
-                return;
-            }
-            if (String.valueOf(passwordTextField.getPassword()).isEmpty()) {
-                passwordErrorLabel.setText("Password cannot be empty");
-                passwordErrorLabel.setVisible(true);
-                return;
-            }
-            if (emailTextField.getText().equals("admin") && String.valueOf(passwordTextField.getPassword()).equals("admin")) {
-                JOptionPane.showMessageDialog(null, "Login Successful", "Success", JOptionPane.INFORMATION_MESSAGE);
-            } else {
-                JOptionPane.showMessageDialog(null, "Invalid Credentials", "Error", JOptionPane.ERROR_MESSAGE);
-            }
-        });
-        signInButton.addActionListener(e -> JOptionPane.showMessageDialog(null, "Sign In Successful", "Success", JOptionPane.INFORMATION_MESSAGE));
+        loginButton.addActionListener(e -> onLogin());
+        signUpButton.addActionListener(e -> onSignUp());
 
         // assign error label listeners
         emailTextField.addFocusListener(new java.awt.event.FocusAdapter() {
@@ -53,6 +48,47 @@ public class LoginView {
                 passwordErrorLabel.setVisible(false);
             }
         });
+    }
+
+    public void onLogin() {
+        if (emailTextField.getText().isEmpty()) {
+            emailErrorLabel.setText("Email cannot be empty");
+            emailErrorLabel.setVisible(true);
+            return;
+        }
+        if (String.valueOf(passwordTextField.getPassword()).isEmpty()) {
+            passwordErrorLabel.setText("Password cannot be empty");
+            passwordErrorLabel.setVisible(true);
+            return;
+        }
+        // check if user email is in the database
+        String sql = "SELECT * FROM users WHERE email = '" + emailTextField.getText() + "';";
+        try (Connection conn = DriverManager.getConnection(DatabaseSetup.databaseUrl);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            // check if user email is in the database
+            if (!rs.next()) {
+                emailErrorLabel.setText("Incorrect email or password");
+                emailErrorLabel.setVisible(true);
+                return;
+            }
+            // check if password is correct
+            if (!Password.check(String.valueOf(passwordTextField.getPassword()), rs.getString("password_hash")).withBcrypt()) {
+                passwordErrorLabel.setText("Incorrect email or password");
+                passwordErrorLabel.setVisible(true);
+                return;
+            }
+            // login successful
+            JOptionPane.showMessageDialog(null, "Login Successful", "Success", JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void onSignUp() {
+        SignUpView signUpView = new SignUpView();
+        signUpView.pack();
+        signUpView.setVisible(true);
     }
 
     public JPanel getMainPanel() {
