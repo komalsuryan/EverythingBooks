0c21aff91d51d574cf04392406fea08366c4ad14
Amisha Kalhan <64070890+amisha109@users.noreply.github.com>
2022-12-12 04:49:57 +0530

    :wrench: Add PublishingCompanyAdminView Skeleton

---
+++ src/main/java/org/amishaandkomal/views/PublishingCompanyAdminView.java
@@ -0,0 +1,11 @@
+package org.amishaandkomal.views;
+
+import javax.swing.*;
+
+public class PublishingCompanyAdminView {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+}
