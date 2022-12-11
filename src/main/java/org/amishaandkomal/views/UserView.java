be1ae8ddf5c03f285c9cddd2a0f8adad2463942c
Amisha Kalhan <64070890+amisha109@users.noreply.github.com>
2022-12-12 04:48:57 +0530

    :wrench: Add UserView skeleton

---
+++ src/main/java/org/amishaandkomal/views/UserView.java
@@ -0,0 +1,10 @@
+package org.amishaandkomal.views;
+
+import javax.swing.*;
+
+public class UserView {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JButton button1;
+    private JButton button2;
+}
