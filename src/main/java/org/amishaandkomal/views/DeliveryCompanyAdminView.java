2b41b520badf3876aa6f0675c2fbc29935af9bdd
Amisha Kalhan <64070890+amisha109@users.noreply.github.com>
2022-12-12 04:51:11 +0530

    :wrench: Add DeliveryCompanyAdminView Skeleton

---
+++ src/main/java/org/amishaandkomal/views/DeliveryCompanyAdminView.java
@@ -0,0 +1,11 @@
+package org.amishaandkomal.views;
+
+import javax.swing.*;
+
+public class DeliveryCompanyAdminView {
+    private JPanel panel1;
+    private JTabbedPane tabbedPane1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+}
