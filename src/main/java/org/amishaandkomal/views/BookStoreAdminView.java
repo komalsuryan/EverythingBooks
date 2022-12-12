96bb11ac6f05a45c04d46cda1f8875bf1dce5106
Amisha Kalhan <64070890+amisha109@users.noreply.github.com>
2022-12-12 09:16:48 +0530

    :wrench: Add BookStoreAdminView

---
+++ src/main/java/org/amishaandkomal/views/BookStoreAdminView.java
@@ -0,0 +1,18 @@
+package org.amishaandkomal.views;
+
+import javax.swing.*;
+
+public class BookStoreAdminView {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+
+    public BookStoreAdminView(String email) {
+    }
+
+    public JPanel getMainPanel() {
+        return panel1;
+    }
+}
