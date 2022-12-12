ee342b1b0231d4761a98d468c16b891456d65c43
Amisha Kalhan <64070890+amisha109@users.noreply.github.com>
2022-12-12 09:17:03 +0530

    :wrench: Add BookStoreEmployeeView

---
+++ src/main/java/org/amishaandkomal/views/BookStoreEmployeeView.java
@@ -0,0 +1,17 @@
+package org.amishaandkomal.views;
+
+import javax.swing.*;
+
+public class BookStoreEmployeeView {
+    private JButton button1;
+    private JButton button2;
+    private JTabbedPane tabbedPane1;
+    private JPanel jPanel1;
+
+    public BookStoreEmployeeView(String email) {
+    }
+
+    public JPanel getMainPanel() {
+        return jPanel1;
+    }
+}
